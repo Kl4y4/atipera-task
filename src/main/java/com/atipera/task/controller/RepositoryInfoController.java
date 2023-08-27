fa5f7c4dea1c36c82eeb8525101189c6a5e0c702
Kl4y4 <surowiec.bartosz@wp.pl>
2023-08-28 00:51:07 +0200

    Created basic controller

--- src/main/java/com/atipera/task/controller/RepositoryInfoController.java
+++ src/main/java/com/atipera/task/controller/RepositoryInfoController.java
@@ -1,10 +1,14 @@
 package com.atipera.task.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RepositoryInfoController {
 
-
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world";
+    }
 
 }
