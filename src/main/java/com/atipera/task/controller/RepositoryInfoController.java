e9b92c4204913dfa9d23071106efcc60c10e65f1
Kl4y4 <surowiec.bartosz@wp.pl>
2023-08-29 01:06:00 +0200

    Changed to ResponseEntity objects for returning responses

--- src/main/java/com/atipera/task/controller/RepositoryInfoController.java
+++ src/main/java/com/atipera/task/controller/RepositoryInfoController.java
@@ -4,6 +4,7 @@ import com.atipera.task.service.APIConsumerService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,24 +14,23 @@ public class RepositoryInfoController {
     private final APIConsumerService apiService;
     private final ObjectMapper objectMapper;
 
-    @GetMapping("/")
-    public String hello() {
-        return apiService.GetHello();
-    }
-
     @GetMapping("/repos")
-    public String GetRepos(@RequestParam String username, @RequestHeader("Accept") String acceptHeader) throws JsonProcessingException {
+    public ResponseEntity<String> getRepos(@RequestParam String username, @RequestHeader("Accept") String acceptHeader) throws JsonProcessingException {
 
-        if (!CheckHeaders(acceptHeader)) {
-            System.out.println("406");
-//            return objectMapper.writeValueAsString(new ErrorSchema(406, "Not Acceptable - Accept header does not contain 'application/json'"));
+        if (!checkHeaders(acceptHeader)) {
+            return ResponseEntity.status(406).body("Not Acceptable - Accept header does not contain 'application/json'");
         }
 
-        return apiService.GetRepos(username);
+        try {
+            return ResponseEntity.status(200).body(apiService.getRepos(username));
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body("Not Found - This user does not exist");
+        }
+
     }
 
-    private boolean CheckHeaders(String acceptHeader) {
-        return acceptHeader.contains("application/json");
+    private boolean checkHeaders(String acceptHeader) {
+        return acceptHeader.toLowerCase().contains("application/json");
     }
 
 }
