de7931add061763c5bc3c04dc5d13a2e2571204d
Kl4y4 <surowiec.bartosz@wp.pl>
2023-08-28 16:59:42 +0200

    Added endpoint to retrieve mine repos

--- src/main/java/com/atipera/task/controller/RepositoryInfoController.java
+++ src/main/java/com/atipera/task/controller/RepositoryInfoController.java
@@ -1,14 +1,24 @@
 package com.atipera.task.controller;
 
+import com.atipera.task.service.APIConsumerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RepositoryInfoController {
+
+    private final APIConsumerService apiService;
 
     @GetMapping("/")
     public String hello() {
-        return "Hello world";
+        return apiService.GetHello();
+    }
+
+    @GetMapping("/repos")
+    public String GetRepos() {
+        return apiService.GetRepos();
     }
 
 }
--- src/main/java/com/atipera/task/service/APIConsumerService.java
+++ src/main/java/com/atipera/task/service/APIConsumerService.java
@@ -1,7 +1,40 @@
 package com.atipera.task.service;
 
+import com.atipera.task.model.RepoResponseObj;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+@Scope
+@RequiredArgsConstructor
 public class APIConsumerService {
 
+    private final RestTemplate restTemplate;
 
+    public String GetHello() {
+        return "Hello world!";
+    }
+
+    public String GetRepos() {
+
+        RepoResponseObj[] response = restTemplate.getForObject(
+                "https://api.github.com/users/kl4y4/repos", RepoResponseObj[].class
+        );
+
+        response = Arrays.stream(response).filter(res -> !res.fork()).toArray(RepoResponseObj[]::new);
+
+        StringBuilder sb = new StringBuilder();
+        for (RepoResponseObj repo : response) {
+            sb.append(repo.name())
+                    .append(" ")
+                    .append(repo.owner().login());
+        }
+
+        return sb.toString();
+    }
 
 }
