9db5000b2e67215bee8612a1fd0e7e08929094c7
Kl4y4 <surowiec.bartosz@wp.pl>
2023-08-28 19:44:14 +0200

    API now serves repos depending on user, response returned in json

--- src/main/java/com/atipera/task/service/APIConsumerService.java
+++ src/main/java/com/atipera/task/service/APIConsumerService.java
@@ -1,12 +1,18 @@
 package com.atipera.task.service;
 
-import com.atipera.task.model.RepoResponseObj;
+import com.atipera.task.model.Branch;
+import com.atipera.task.model.RepoExternalObj;
+import com.atipera.task.model.ResponseObj;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Scope
@@ -14,27 +20,29 @@ import java.util.Arrays;
 public class APIConsumerService {
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     public String GetHello() {
         return "Hello world!";
     }
 
-    public String GetRepos() {
+    public String GetRepos(String username) throws JsonProcessingException {
 
-        RepoResponseObj[] response = restTemplate.getForObject(
-                "https://api.github.com/users/kl4y4/repos", RepoResponseObj[].class
+        RepoExternalObj[] response = restTemplate.getForObject(
+                "https://api.github.com/users/" + username + "/repos", RepoExternalObj[].class
         );
 
-        response = Arrays.stream(response).filter(res -> !res.fork()).toArray(RepoResponseObj[]::new);
+        response = Arrays.stream(response).filter(res -> !res.fork()).toArray(RepoExternalObj[]::new);
 
-        StringBuilder sb = new StringBuilder();
-        for (RepoResponseObj repo : response) {
-            sb.append(repo.name())
-                    .append(" ")
-                    .append(repo.owner().login());
+        List<ResponseObj> responseObjs = new ArrayList<>();
+        for (RepoExternalObj repo : response) {
+            ResponseObj obj = new ResponseObj(
+                    repo.name(), repo.owner().login(), new Branch[]{}
+            );
+            responseObjs.add(obj);
         }
 
-        return sb.toString();
+        return objectMapper.writeValueAsString(responseObjs);
     }
 
 }
