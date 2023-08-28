56eb861ed493263f17b0973f04f6fc15bcc62fac
Kl4y4 <surowiec.bartosz@wp.pl>
2023-08-28 22:30:41 +0200

    Added ability to serve branches

--- src/main/java/com/atipera/task/service/APIConsumerService.java
+++ src/main/java/com/atipera/task/service/APIConsumerService.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 @Service
 @Scope
@@ -40,9 +42,39 @@ public class APIConsumerService {
 
         response = Arrays.stream(response).filter(res -> !res.fork()).toArray(RepoExternalObj[]::new);
 
-        List<ResponseObj> repos = Arrays.stream(response).map(res -> new ResponseObj(res.name(), res.owner().login(), new Branch[] {})).toList();
+        List<Branch[]> branches = FetchBranches(Arrays.stream(response).toList());
+        ListIterator<Branch[]> branchListIterator = branches.listIterator();
+
+        List<ResponseObj> repos = Arrays.stream(response).map(res -> new ResponseObj(res.name(), res.owner().login(), branchListIterator.next())).toList();
 
         return repos;
+    }
+
+    private List<Branch[]> FetchBranches(List<RepoExternalObj> response) {
+
+        List<Branch[]> branches = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (RepoExternalObj respObj : response) {
+
+            sb.append("https://api.github.com/repos/")
+                    .append(respObj.owner().login())
+                    .append("/")
+                    .append(respObj.name())
+                    .append("/branches");
+
+            Branch[] branchesToAdd = restTemplate.getForObject(
+                    sb.toString(), Branch[].class
+            );
+
+            branches.add(branchesToAdd);
+
+            sb.setLength(0);
+
+        }
+
+        return branches;
     }
 
 }
